57cb4e75ee622f1a330fc8944fc72b6978486707
Michael Flaxman <mflaxman@gmail.com>
2015-11-17 15:47:13 -0500

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
     from distutils.core import setup
 
 setup(name='secondguard',
-      version='1.1.0',
+      version='1.1.1',
       description='SecondGuard API & Encryption Library',
       author='Michael Flaxman',
       author_email='mflaxman+secondguard@gmail.com',
